cfeb19190fff97ecfdba4374527b8326fc1d8523
Yiyoung <yiyoung.md@gmail.com>
2025-11-25 13:28:56 -0500

    feat: basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 int main() {
-  std::cout << "Hello World!\n";
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Window");
+
+  while (window.isOpen()) {
+    while(const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+
+    window.clear(sf::Color::Black);
+
+    window.display();
+  }
+
   return 0;
 }
