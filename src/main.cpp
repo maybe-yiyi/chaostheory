b661fe00aa6b1c6e1b85fb4c961315702d8cfd36
Yiyoung <yiyoung.md@gmail.com>
2025-11-27 23:20:31 -0500

    lorenz attractor for one particle

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <functional>
 
 #include <SFML/Graphics.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat3x3.hpp>
+
+class Particle {
+  glm::vec3 position;
+
+public:
+  Particle(const glm::vec3& pos)
+      : position(pos) {}
+
+  const glm::vec3& getPosition() const {
+    return position;
+  }
+
+  void update(const std::function<glm::vec3(const glm::vec3&, float)>& dynamics, float dt) {
+    position = dynamics(position, dt);
+  }
+};
 
 int main() {
   sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Window", sf::State::Fullscreen);
 
+  Particle particle({1.f, 1.f, 0.1f});
+  auto lorenzattractor = [&](const glm::vec3& pos, const float dt) {
+    const float sigma = 10.0f;
+    const float rho = 28.0f;
+    const float beta = 8.0f / 3.0f;
+
+    glm::vec3 dpos;
+    dpos.x = sigma * (pos.y - pos.x);
+    dpos.y = pos.x * (rho - pos.z) - pos.y;
+    dpos.z = pos.x * pos.y - beta * pos.z;
+
+    return pos + dpos * dt;
+  };
+
+  sf::Clock clock;
   while (window.isOpen()) {
+    float dt = clock.restart().asSeconds();
+    particle.update(lorenzattractor, dt);
+
     while(const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>()) {
         window.close();
@@ -19,6 +56,11 @@ int main() {
 
     window.clear(sf::Color::Black);
 
+    sf::CircleShape shape(5);
+    shape.setFillColor(sf::Color::White);
+    shape.setPosition({ 960 + particle.getPosition().x * 10, 540 + particle.getPosition().y * 10 });
+
+    window.draw(shape);
     window.display();
   }
 
