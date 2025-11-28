537fd2f34a169483e4ff8eff12b5322cc3387d4a
Yiyoung <yiyoung.md@gmail.com>
2025-11-27 23:19:59 -0500

    made window fullscreen (resolution is my resolution for now)

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,18 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode({800, 600}), "Window");
+  sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Window", sf::State::Fullscreen);
 
   while (window.isOpen()) {
     while(const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>())
+      if (event->is<sf::Event::Closed>()) {
         window.close();
+      } else if (event->is<sf::Event::KeyPressed>()) {
+        const auto* keyEvent = event->getIf<sf::Event::KeyPressed>();
+        if (keyEvent->code == sf::Keyboard::Key::Escape) {
+          window.close();
+        }
+      }
     }
 
     window.clear(sf::Color::Black);
